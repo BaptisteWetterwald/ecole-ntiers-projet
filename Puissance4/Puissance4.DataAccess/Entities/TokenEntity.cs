d7552b32cc976a6a9cc77dbe490c631044e9171c
Baptiste Wetterwald <baptiste.wetterwald@gmail.com>
2025-01-20 14:00:52 +0100

    Replaced init with set for TokenEntity

--- Puissance4/Puissance4.DataAccess/Entities/TokenEntity.cs
+++ Puissance4/Puissance4.DataAccess/Entities/TokenEntity.cs
@@ -1,8 +1,8 @@
 ﻿namespace Puissance4.DataAccess.Entities;
 
-public class TokenEntity()
+public class TokenEntity
 {
-    public int Id { get; init; }
+    public int Id { get; set; }
     public int CellId { get; set; }
     public required string Color { get; init; }
 }
